a1cbeca743b07152692c2d805a717aa8c7b3dfb0
Pietro <pietrolubrano86@gmail.com>
2026-01-20 22:41:43 +0100

    new Navbar

--- app/(components)/Navbar.tsx
+++ app/(components)/Navbar.tsx
@@ -1,181 +1,144 @@
 "use client"
 
-import Image from 'next/image';
-import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
-import { Bars3Icon, ChevronDownIcon, XMarkIcon } from '@heroicons/react/24/outline'
-import Link from 'next/link';
+import Image from "next/image";
+import React from "react";
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarMenuToggle,
+  NavbarMenuItem,
+  NavbarMenu,
+  NavbarContent,
+  NavbarItem,
+  Link,
+  Button,
+  DropdownTrigger,
+  Dropdown,
+  DropdownMenu,
+  DropdownItem,
+} from "@heroui/react";
 
 const navigation = [
-  { name: 'Dashboard', href: '#', current: true },
-  { name: 'Team', href: '#', current: false },
-  { name: 'Projects', href: '#', current: false },
-  { name: 'Calendar', href: '#', current: false },
+  { name: 'Chi siamo', href: '/about' },
+  { name: 'Contatti', href: '/contacts' }
 ]
+
+const rooms = [
+  { name: 'San Biagio', href: '/room/san-biagio' },
+  { name: 'Maiorani', href: '/room/maiorani' },
+  { name: 'Divino Amore', href: '/room/divino-amore' }
+]
+
+export const Logo = () => {
+  return (
+    <Image
+      width={100}
+      height={100}
+      loading="eager"
+      alt="Your Company"
+      src="/images/napoli-napoli-logo.png"
+      className="h-8 w-auto"
+    />
+  );
+};
+
+export default function App() {
+  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+
+  const menuItems = [
+    "Profile",
+    "Dashboard",
+    "Activity",
+    "Analytics",
+    "System",
+    "Deployments",
+    "My Settings",
+    "Team Settings",
+    "Help & Feedback",
+    "Log Out",
+  ];
+
+  return (
+    <Navbar isBordered isMenuOpen={isMenuOpen} onMenuOpenChange={setIsMenuOpen} className="bg-background z-20">
+
+      <NavbarContent className="sm:hidden pr-3" justify="center">
+        <NavbarBrand>
+          <Link href="/">
+            <Logo />
+          </Link>
+        </NavbarBrand>
+      </NavbarContent>
+
+      <NavbarContent className="sm:hidden" justify="end">
+        <NavbarMenuToggle aria-label={isMenuOpen ? "Close menu" : "Open menu"} />
+      </NavbarContent>
+
+      <NavbarContent className="hidden sm:flex gap-4" justify="center">
+        <NavbarBrand>
+          <Link href="/">
+            <Logo />
+          </Link>
+        </NavbarBrand>
+        
+        <Dropdown>
+          <NavbarItem>
+            <DropdownTrigger>
+              <Button
+                
+                className="p-0 bg-transparent data-[hover=true]:bg-transparent text-white"
+                radius="sm"
+              >
+                Rooms
+              </Button>
+            </DropdownTrigger>
+          </NavbarItem>
+          <DropdownMenu
+            aria-label="ACME features"
+            itemClasses={{
+              base: "gap-4",
+            }}
+          >
+            {rooms.map((item) => (
+              <DropdownItem key={item.name} as={Link} href={item.href}>
+                {item.name}
+              </DropdownItem>
+            ))}
+          </DropdownMenu>
+        </Dropdown>
+        {
+          navigation.map((item) => (
+            <NavbarItem key={item.name} className={classNames(
+              "text-inherit hover:text-inherit",
+            )}>
+              <Link className="text-white" href={item.href}>
+                {item.name}
+              </Link>
+            </NavbarItem>
+          ))
+        }
+      </NavbarContent>
+
+      <NavbarMenu className="bg-background">
+        {navigation.map((item, index) => (
+          <NavbarMenuItem key={`${item.name}-${index}`}>
+            <Link
+              className="w-full text-white"
+           /*    color={
+                index === 2 ? "primary" : index === navigation.length - 1 ? "danger" : "foreground"
+              } */
+              href={item.href}
+              size="lg"
+            >
+              {item.name}
+            </Link>
+          </NavbarMenuItem>
+        ))}
+      </NavbarMenu>
+    </Navbar>
+  );
+}
 
 function classNames(...classes: string[]) {
   return classes.filter(Boolean).join(' ')
 }
 
-export default function Navbar() {
-  return (
-    <Disclosure
-      as="nav"
-      className="relative text-white font-bold bg-background"
-    >
-      <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-        <div className="relative flex h-16 items-center justify-between">
-          <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-            {/* Mobile menu button*/}
-            <DisclosureButton className="group relative inline-flex items-center justify-center rounded-md p-2 text-gray-800 hover:bg-white/5 hover:text-white focus:outline-2 focus:-outline-offset-1 focus:outline-indigo-500">
-              <span className="absolute -inset-0.5" />
-              <span className="sr-only">Open main menu</span>
-              <Bars3Icon aria-hidden="true" className="block size-6 group-data-open:hidden" />
-              <XMarkIcon aria-hidden="true" className="hidden size-6 group-data-open:block" />
-            </DisclosureButton>
-          </div>
-          <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-            <div className="flex shrink-0 items-center">
-              <Link href={'/'}>
-                <Image
-                  width={100}
-                  height={100}
-                  loading="eager"
-                  alt="Your Company"
-                  src="/images/napoli-napoli-logo.png"
-                  className="h-8 w-auto"
-                />
-              </Link>
-              
-            </div>
-            <div className="hidden sm:ml-6 sm:block">
-              <div className="flex space-x-4">
-
-                <Menu as="div" className="relative inline-block">
-                  <MenuButton className="inline-flex w-full justify-center gap-x-1.5 rounded-md px-3 py-2 text-sm font-semibold text-white hover:bg-white/20">
-                    ROOMS
-                    <ChevronDownIcon aria-hidden="true" className="-mr-1 size-5 text-white" />
-                  </MenuButton>
-
-                  <MenuItems
-                    transition
-                    className="absolute left-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-background outline-1 -outline-offset-1 outline-white/10 transition data-closed:scale-95 data-closed:transform data-closed:opacity-0 data-enter:duration-100 data-enter:ease-out data-leave:duration-75 data-leave:ease-in"
-                  >
-                    <div className="py-1">
-                      {
-                        [
-                          {name: 'San Biagio', uri: '/room/san-biagio'},
-                          {name: 'Maiorani', uri: '/room/maiorani'},
-                          {name: 'Divino Amore', uri: '/room/divino-amore'}
-                        ].map(room => 
-                          <MenuItem key={room.name}>
-                            <Link
-                              href={room.uri}
-                              className="block px-4 py-2 text-sm text-white data-focus:bg-white/5 data-focus:text-white data-focus:outline-hidden"
-                            >
-                              {room.name}
-                            </Link>
-                          </MenuItem>
-                        )
-                      }
-                      
-                    </div>
-                  </MenuItems>
-                </Menu>
-
-                {navigation.map((item) => (
-                  <a
-                    key={item.name}
-                    href={item.href}
-                    aria-current={item.current ? 'page' : undefined}
-                    className={classNames(
-                      item.current
-                        ? ''
-                        : ' hover:bg-white/5 hover:text-background',
-                      'rounded-md px-3 py-2 text-sm font-bold uppercase ',
-                    )}
-                  >
-                    {item.name}
-                  </a>
-                ))}
-              </div>
-            </div>
-          </div>
-          <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-            {/* <button
-              type="button"
-              className="relative rounded-full p-1 text-gray-400 focus:outline-2 focus:outline-offset-2 focus:outline-indigo-500"
-            >
-              <span className="absolute -inset-1.5" />
-              <span className="sr-only">View notifications</span>
-              <BellIcon aria-hidden="true" className="size-6" />
-            </button> */}
-
-            {/* Profile dropdown */}
-            <Menu as="div" className="relative ml-3">
-              <MenuButton className="relative flex rounded-full focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-background">
-                <span className="absolute -inset-1.5" />
-                <span className="sr-only">Open user menu</span>
-                <img
-                  alt=""
-                  src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                  className="size-8 rounded-full bg-gray-800 outline -outline-offset-1 outline-white/10"
-                />
-              </MenuButton>
-
-              <MenuItems
-                transition
-                className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg outline outline-black/5 transition data-closed:scale-95 data-closed:transform data-closed:opacity-0 data-enter:duration-100 data-enter:ease-out data-leave:duration-75 data-leave:ease-in"
-              >
-                <MenuItem>
-                  <a
-                    href="#"
-                    className="block px-4 py-2 text-sm text-gray-700 data-focus:bg-gray-100 data-focus:outline-hidden"
-                  >
-                    Your profile
-                  </a>
-                </MenuItem>
-                <MenuItem>
-                  <a
-                    href="#"
-                    className="block px-4 py-2 text-sm text-gray-700 data-focus:bg-gray-100 data-focus:outline-hidden"
-                  >
-                    Settings
-                  </a>
-                </MenuItem>
-                <MenuItem>
-                  <a
-                    href="#"
-                    className="block px-4 py-2 text-sm text-gray-700 data-focus:bg-gray-100 data-focus:outline-hidden"
-                  >
-                    Sign out
-                  </a>
-                </MenuItem>
-              </MenuItems>
-            </Menu>
-          </div>
-        </div>
-      </div>
-
-      <DisclosurePanel className="sm:hidden">
-        <div className="space-y-1 px-2 pt-2 pb-3">
-          {navigation.map((item) => (
-            <DisclosureButton
-              key={item.name}
-              as="a"
-              href={item.href}
-              aria-current={item.current ? 'page' : undefined}
-              className={classNames(
-                item.current
-                  ? 'bg-gray-900 text-white'
-                  : 'text-gray-300 hover:bg-white/5 hover:text-white',
-                'block rounded-md px-3 py-2 text-base font-medium',
-              )}
-            >
-              {item.name}
-            </DisclosureButton>
-          ))}
-        </div>
-      </DisclosurePanel>
-    </Disclosure>
-  )
-}
