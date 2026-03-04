2ff967eff758e8acd2835bfb182cb548352408ee
Pietro <pietrolubrano86@gmail.com>
2026-03-04 23:37:31 +0100

    sitemap language added

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -7,26 +7,56 @@ export default function sitemap(): MetadataRoute.Sitemap {
       lastModified: new Date(),
       changeFrequency: 'yearly',
       priority: 1,
+      alternates: {
+        languages: {
+          it: 'https://www.napolinapolirooms.it/it',
+          en: 'https://www.napolinapolirooms.it/en',
+        },
+      },
     },{
       url: 'https://www.napolinapolirooms.it/search',
       lastModified: new Date(),
       changeFrequency: 'yearly',
       priority: 0.9,
+      alternates: {
+        languages: {
+          it: 'https://www.napolinapolirooms.it/it/search',
+          en: 'https://www.napolinapolirooms.it/en/search',
+        },
+      },
     },{
       url: 'https://www.napolinapolirooms.it/room/san-biagio',
       lastModified: new Date(),
       changeFrequency: 'yearly',
       priority: 0.8,
+      alternates: {
+        languages: {
+          it: 'https://www.napolinapolirooms.it/it/room/san-biagio',
+          en: 'https://www.napolinapolirooms.it/en/room/san-biagio',
+        },
+      },
     },{
       url: 'https://www.napolinapolirooms.it/room/maiorani',
       lastModified: new Date(),
       changeFrequency: 'yearly',
       priority: 0.8,
+      alternates: {
+        languages: {
+          it: 'https://www.napolinapolirooms.it/it/room/maiorani',
+          en: 'https://www.napolinapolirooms.it/en/room/maiorani',
+        },
+      },
     },{
       url: 'https://www.napolinapolirooms.it/room/divino-amore',
       lastModified: new Date(),
       changeFrequency: 'yearly',
       priority: 0.8,
+      alternates: {
+        languages: {
+          it: 'https://www.napolinapolirooms.it/it/room/divino-amore',
+          en: 'https://www.napolinapolirooms.it/en/room/divino-amore',
+        },
+      },
     }
   ]
 }
