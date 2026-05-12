d558377be53e292e0938bdfa973e2f91cc20584d
Pietro <pietrolubrano86@gmail.com>
2026-05-12 14:50:53 +0200

    new tripadvisor widget

--- app/[lang]/components/TripAdvisor.tsx
+++ app/[lang]/components/TripAdvisor.tsx
@@ -6,15 +6,16 @@ import Link from "next/link";
 
 export default function TripAdvisor() {
 
-    return (<>
-        <div id="TA_selfserveprop838" className="TA_selfserveprop">
-            <ul id="FD0Wp8" className="TA_links xHuZmONpB">
-                <li id="WEKgBovRqdk" className="p674QANNOAek">
+    return(<>
+        <div id="TA_cdsratingsonlynarrow290" className="TA_cdsratingsonlynarrow max-w-34">
+            <ul id="HAfVeLjBE6b" className="TA_links f9lMaUxDs9A">
+                <li id="Nk6YXVd" className="CSMhm2M">
                     <Link target="_blank" href="https://www.tripadvisor.it/Hotel_Review-g187785-d1152912-Reviews-Napoli_Napoli-Naples_Province_of_Naples_Campania.html">
-                        <Image width={200} height={200} src="https://www.tripadvisor.it/img/cdsi/img2/branding/v2/Tripadvisor_lockup_horizontal_secondary_registered-11900-2.svg" alt="TripAdvisor"/>
+                        <Image width={200} height={200} src="https://www.tripadvisor.it/img/cdsi/img2/branding/v2/Tripadvisor_lockup_horizontal_secondary_registered-18034-2.svg" alt="TripAdvisor"/>
                     </Link>
                 </li>
             </ul>
         </div>
-        <Script async strategy="afterInteractive" src="https://www.jscache.com/wejs?wtype=selfserveprop&amp;uniq=838&amp;locationId=1152912&amp;lang=it&amp;rating=true&amp;nreviews=0&amp;writereviewlink=false&amp;popIdx=false&amp;iswide=false&amp;border=true&amp;display_version=2" data-loadtrk></Script>
-    </>)}
+        <Script async strategy="afterInteractive" src="https://www.jscache.com/wejs?wtype=cdsratingsonlynarrow&amp;uniq=290&amp;locationId=1152912&amp;lang=it&amp;border=true&amp;shadow=false&amp;backgroundColor=white&amp;display_version=2" data-loadtrk></Script>
+    </>)
+}
