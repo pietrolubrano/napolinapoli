4869baa611f67931ead1dbe888a396f896b9512f
Pietro <pietrolubrano86@gmail.com>
2026-05-13 15:40:04 +0200

    cookies expires in 400 days

--- app/actions/cookieActions.ts
+++ app/actions/cookieActions.ts
@@ -3,9 +3,10 @@
 import { cookies } from "next/headers"
 
 export const setReservationCookie = async (reservationId: string, email: string) => {
+    const expirationTime = new Date(Date.now() + 400 * 24 * 60 * 60 * 1000); // expires in 400 days
     const cookieStore = await cookies()
-    cookieStore.set('reservationId', reservationId)
-    cookieStore.set('email', email)
+    cookieStore.set('reservationId', reservationId, {expires: expirationTime})
+    cookieStore.set('email', email, {expires: expirationTime})
 }
 
 export const clearReservationCookie = async () => {
